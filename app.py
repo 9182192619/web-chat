d192653467e7dfd6fec110a7b8940ed8eeaca769
Vijay <ekilavijay@gmail.com>
2025-12-17 09:09:03 +0530

    Format socketio.run parameters for improved readability

--- app.py
+++ app.py
@@ -197,4 +197,10 @@ def disconnect():
 
 if __name__ == "__main__":
     init_db()
-    socketio.run(app, host="0.0.0.0",port=10000)
+    socketio.run(
+    app,
+    host="0.0.0.0",
+    port=10000,
+    allow_unsafe_werkzeug=True
+)
+
